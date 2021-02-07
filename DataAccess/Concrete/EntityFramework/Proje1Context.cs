fd18c825c3cdd637e6ff472aea2311a1dac7a4a4
Beril Özbay <beril_ozbay@hotmail.com>
2021-02-07 21:29:58 +0300

    EntityFramework

---
+++ DataAccess/Concrete/EntityFramework/Proje1Context.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class Proje1Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ProjeDatabase1;Trusted_Connection=true");
+        }
+        public DbSet<Car> Cars{ get; set; }
+        public DbSet <Color> Colors { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
